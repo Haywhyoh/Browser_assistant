b53cd316256436aeedbf97025b07bff516687fc7
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-12 23:32:11 +0100

    just dey play

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
 import './popup.css';
+
 const test = (
   <div class="main">
-     <div ><img className="feature_icon" src="./icon_48.png" /></div>
+    <div ><img className="feature_icon" src="./icon_48.png" /></div>
     <div ><img className="feature_icon" src="./chat.svg" /></div>
     <div ><img className="feature_icon" src="./games.svg" /></div>
     <div ><img className="feature_icon" src="./screenshot_48.svg" /></div>
