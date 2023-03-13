3ba71a38761e6ddb46cbf44a7dabd9105f3b0f85
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-13 20:05:09 +0100

    created the screenshot component

--- src/components/summarise/summarise.js
+++ src/components/summarise/summarise.js
@@ -1,5 +1,10 @@
+import Nav from "../Navigation/navigation"
+
 export default function Summarise() {
   return (
-    <div></div>
+    <div>
+      <Nav />
+      <h2>Summarise</h2>
+    </div>
   )
 }
