7220f81750953a9d9105029dedb1cf79cf362893
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-13 20:05:15 +0100

    created the utube component

--- src/components/screenshot/screenshot.js
+++ src/components/screenshot/screenshot.js
@@ -1,5 +1,9 @@
+import Nav from "../Navigation/navigation"
 export default function Screenshot() {
   return (
-    <div></div>
+    <div>
+      <Nav />
+      <h2>Screenshot</h2>
+    </div>
   )
 }
