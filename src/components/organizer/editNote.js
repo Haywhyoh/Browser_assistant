30c062799a16616991db0a945808f386a1c6f833
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-15 12:01:14 +0100

    can edit notes

---
+++ src/components/organizer/editNote.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+export default function EditNote() {
+  const location = useLocation()
+  const key = location.state.id
+  const idea = localStorage.getItem(key)
+  const parsedIdea = JSON.parse(idea);
+  const [content, setNote] = useState(parsedIdea.content);
+  const  [title, setTitle] = useState(parsedIdea.title);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+
+  return (
+      <div>
+      <form onSubmit={handleSubmit}>
+        <label>Title <br></br>
+        <input type="text" defaultValue={title} onChange={(e) => setTitle(e.target.value)} id="note_title"/>
+        </label> <br></br>
+        <label>Write Note: 
+        <textarea defaultValue={content} onChange={(e) => setNote(e.target.value)} cols={40} rows={15}  id='note_value'/>
+        </label>
+        <Link to={'/organizer'}><input type="submit" value="Submit" id='set_note' onClick={() => {
+             const title = document.querySelector('#note_title').defaultValue;
+             const content = document.querySelector('#note_value').defaultValue;
+             parsedIdea['title']  = title;
+             parsedIdea['content'] = content;
+             localStorage.setItem(key, JSON.stringify(parsedIdea));
+        }}/></Link>
+      </form>
+    </div>
+  )
+}
+
