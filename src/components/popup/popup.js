d803636d90580cba9d4f3356dbacc8be852569ad
Daniel Obembe <danielobembe1@gmail.com>
2023-03-18 16:22:36 +0100

    crteated styling for the pop-up

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -1,30 +1,35 @@
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./popup.css";
 
 export default function Popup() {
   return (
-    <div>    
-      <h2>Popup</h2>
+    <div className="popup">
       <div>
-        <CustomLink to='/'>Home</CustomLink>
-        <CustomLink to='/organizer'>Organizer</CustomLink>
-        <CustomLink to='/summarise'>Summarise</CustomLink>
-        <CustomLink to='/askchatgpt'>AskChatgpt</CustomLink>
-        <CustomLink to='/utubevid'>UtubeVid</CustomLink>
-        <CustomLink to='/screenshot'>Screenshot</CustomLink>
-        <CustomLink to='/games'>Games</CustomLink>
+        <h2>Browser Assistant</h2>
+        <p>Surf smarter, not harder.</p>
+      </div>
+
+      <div className="popup_items">
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/organizer">Organizer</CustomLink>
+        <CustomLink to="/summarise">Summarise</CustomLink>
+        <CustomLink to="/askchatgpt">AskChatgpt</CustomLink>
+        <CustomLink to="/utubevid">UtubeVid</CustomLink>
+        <CustomLink to="/screenshot">Screenshot</CustomLink>
+        <CustomLink to="/games">Games</CustomLink>
       </div>
     </div>
-  )
+  );
 }
 
-
-function CustomLink({to, children, ...props}) {
+function CustomLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
   return (
-    <li className= {isActive ? "active" : ""} >
-      <Link to={to} {...props} >{children}</Link>
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
     </li>
-    
   );
 }
