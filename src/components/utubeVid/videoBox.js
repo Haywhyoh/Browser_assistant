e9b70168db8e713732bcf18b335a261c7a0daaa3
seunfunmi <aadenekanemmanuel01642127@gmail.com>
2023-03-23 16:54:04 +0000

    created thevideo box component

---
+++ src/components/utubeVid/videoBox.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function VideoBox(props) {
+    const title = props.snippet.title;
+    const imageUrl = props.snippet.thumbnails.default.url;
+    const id = props.id.videoId
+    return (
+        <div>
+        <Link to={'/watch'} state={{id: id}}> 
+            <div><img src={imageUrl} alt={title} /></div>
+            <div>{title}</div>
+        </ Link>
+        </div>
+
+    )
+}
