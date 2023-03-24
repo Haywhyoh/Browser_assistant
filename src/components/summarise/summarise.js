2399a5831609a47a1490ec2d9466930c53ebebe1
Daniel Obembe <danielobembe1@gmail.com>
2023-03-24 03:32:35 +0100

    created the styling required for the remainig fetures

--- src/components/summarise/summarise.js
+++ src/components/summarise/summarise.js
@@ -1,57 +1,82 @@
 import { useState } from "react";
-import{ nanoid } from "nanoid";
+import { nanoid } from "nanoid";
 import { Link } from "react-router-dom";
 
 const { Configuration, OpenAIApi } = require("openai");
 
 export default function Summarise() {
-
-  const neverEver = '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';
+  const neverEver = "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";
   const configuration = new Configuration({
-    apiKey:neverEver,
-    });
+    apiKey: neverEver,
+  });
   const openai = new OpenAIApi(configuration);
 
-  const [prompt, setPrompt] = useState('');
-  const [title,setTitle] = useState('');
-  const [result, setResult] = useState('');
-  
+  const [prompt, setPrompt] = useState("");
+  const [title, setTitle] = useState("");
+  const [result, setResult] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
-  async function getResponse () {
+  async function getResponse() {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: `Summarize this text + ${prompt}`}],
+      messages: [{ role: "user", content: `Summarize this text + ${prompt}` }],
     });
-    const result = completion.data.choices[0].message.content
+    const result = completion.data.choices[0].message.content;
     return setResult(result);
-     }
+  }
   return (
     <div id="summarise_container">
       <form onSubmit={handleSubmit} id="text_form">
-        <label> Summarise your text: 
-        <textarea value= {prompt} cols={40} rows={10} id="input-title" onChange={(e) => { setPrompt(e.target.value)}}/>
+        <label>
+          {" "}
+          Summarise your text:
+          <textarea
+            value={prompt}
+            cols={40}
+            rows={10}
+            id="input-title"
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+          />
         </label>
-        <button type='submit' onClick={() => {const form = document.querySelector('#text_form');
-                                              const save = document.querySelector("#save_button");
-                                              form.style.display = "none";
-                                              getResponse();
-                                              save.style.display="block";
-                                              }}>Submit</button>
+        <button
+          type="submit"
+          onClick={() => {
+            const form = document.querySelector("#text_form");
+            const save = document.querySelector("#save_button");
+            form.style.display = "none";
+            getResponse();
+            save.style.display = "block";
+          }}
+        >
+          Submit
+        </button>
       </form>
       <div>{result}</div>
-      <button id="save_button" style={{display: "none"}} onClick={() =>{ const title = document.querySelector('#input-title').value.slice(0, 10)
-                                                                          const key = nanoid();
-                                                                          const note = { title, content: result, id: key };
-                                                                          const note_value = JSON.stringify(note);
-                                                                          localStorage[key] = note_value;
-                                                                          alert("Saved Successfully!")
-                                                                        }}>Save Chat</button>
+      <button
+        id="save_button"
+        style={{ display: "none" }}
+        onClick={() => {
+          const title = document
+            .querySelector("#input-title")
+            .value.slice(0, 10);
+          const key = nanoid();
+          const note = { title, content: result, id: key };
+          const note_value = JSON.stringify(note);
+          localStorage[key] = note_value;
+          alert("Saved Successfully!");
+        }}
+      >
+        Save Chat
+      </button>
       <Link to={"/organizer"}>
-          <button id="back" style={{display: "none"}}>Back</button>
-        </Link>
+        <button id="back" style={{ display: "none" }}>
+          Back
+        </button>
+      </Link>
     </div>
-  )
+  );
 }
--- src/components/utubeVid/utubevid.js
+++ src/components/utubeVid/utubevid.js
@@ -1,32 +1,48 @@
-import youtube from './request';
-import VideoBox from './videoBox';
+import youtube from "./request";
+import VideoBox from "./videoBox";
+import "./utubevid.css";
 
 import { useState } from "react";
 export default function Utubevid() {
   const [videos, setVideos] = useState([]);
-  const [search, setSearch] = useState('');
-  const  getVideos = async (searchTerm) => {
-      const response = await youtube.get('/search', {
-        params: {
-          q: searchTerm,
-        }
-      });
-      setVideos(response.data.items);
-      console.log(videos)
-    }
+  const [search, setSearch] = useState("");
+  const getVideos = async (searchTerm) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: searchTerm,
+      },
+    });
+    setVideos(response.data.items);
+    console.log(videos);
+  };
 
   return (
-    <div>
-     <form onSubmit={(e) => e.preventDefault()}>
-      <input type="text" value={search} id="search_val" onChange={(e) => {setSearch(e.target.value)}}/> 
-      <button type='submit' onClick={() =>{const searchVal = document.querySelector("#search_val").value; getVideos(searchVal)} } >Search</button>
-     </form>
-     <div>
-      {videos.map((video) => (
-        <VideoBox key={video.etag} {...video} />
-      ))}
-     </div>
-
+    <div className="utube_main">
+      <h2 className="utube_title">Search your favorite videos</h2>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={search}
+          id="search_val"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          onClick={() => {
+            const searchVal = document.querySelector("#search_val").value;
+            getVideos(searchVal);
+          }}
+        >
+          Search
+        </button>
+      </form>
+      <div className="utube_result">
+        {videos.map((video) => (
+          <VideoBox key={video.etag} {...video} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
